43b63cad3b6397cc7c5dca379f1b22b0e273acf2
alexanderleonardo228-web <alexanderleonardo228@gmail.com>
2026-05-07 15:48:26 +0000

    Wiping all files

--- .ccls-cache/@home@runner@workspace/display.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-void showStatus(int sol, int oxygen, int food, int power);
-
-#endif
--- .ccls-cache/@home@runner@workspace/input.h
+++
@@ -1,6 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-void getPlayerChoice(int &hOxygen, int &hFood, int &hPower);
-
-#endif
